00ea646d69f9cd2d2bab9e338289240f52ad6bde
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-10-15 14:47:35 +0530

    Add : Javascript logic to make the wolf move towards the player and the player to jump

---
+++ script.js
@@ -0,0 +1,31 @@
+// Jumping Game
+const hero = document.querySelector(".hero");
+const heroBoy = document.querySelector(".hero-boy");
+const villian = document.querySelector(".villian");
+
+const jump = () => {
+    if (!hero.classList != "animate") {
+        hero.classList.add("animate");
+        villian.style.animation = "move 1s infinite linear";
+    }
+    setTimeout(() => {
+        hero.classList.remove("animate");
+    }, 300);
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.code == "Space") {
+        jump();
+    }
+})
+
+
+let isAlive = setInterval(() => {
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    let villianLeft = parseInt(window.getComputedStyle(villian).getPropertyValue("left"));
+
+    if (villianLeft < 40 && villianLeft > 20 && heroTop >= 130) {
+        villian.style.animation = "none";
+        alert("Game over. Press spacebar to start");
+    }
+}, 10);
