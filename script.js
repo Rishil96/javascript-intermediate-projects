041a1f1d98b137c5a81c3f7efc679a724a813553
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-10-21 19:27:02 +0530

    Add : JavaScript logic for GitHub Profile Search app

---
+++ script.js
@@ -0,0 +1,15 @@
+import * as v from "./js/variables.js";
+import { getUser, errorMessage } from "./js/functions.js";
+
+v.form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let user = v.search.value.split(" ").join("");
+
+    if (user === "") {
+        errorMessage("Input cannot be blank");
+    }
+    else {
+        getUser(user);
+        v.form.reset();
+    }
+});
